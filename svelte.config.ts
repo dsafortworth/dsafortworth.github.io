2d48f0c1039f7126f1b487631b5c33c6b564e34e
Idigo Luwum <luwum@pm.me>
2025-06-23 14:33:59 -0500

    feat: github-pages action added

---
+++ svelte.config.ts
@@ -0,0 +1,28 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-cloudflare';
+import type { Config } from '@sveltejs/kit';
+
+export default {
+	// Consult https://svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		adapter: adapter({
+			// See https://kit.svelte.dev/docs/adapter-cloudflare for more information
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
+		}),
+
+		// Set the output directory to ./dist at the root of the project
+		outDir: './dist',
+		// Optional: if you want to manually set the directory that gets aliased as `$lib`
+		alias: {
+			$lib: './src/lib'
+		}
+	}
+} satisfies Config;
