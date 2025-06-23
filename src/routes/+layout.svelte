ca3815f4b4bdefe8e0f80cc7538bee7978dd8875
Idigo Luwum <luwum@pm.me>
2025-06-23 13:46:14 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	//import CalenderEmbed from '../components/CalenderEmbed.svelte';
+	import NavigationBar from '../components/NavigationBar.svelte';
+
+	let { children } = $props();
+</script>
+
+<NavigationBar></NavigationBar>
+
+
+{@render children()}
+
