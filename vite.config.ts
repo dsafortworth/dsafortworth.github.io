8c67cda10cfc3ff305034e50cb04be5e68675c5a
Idigo Luwum <luwum@pm.me>
2025-06-26 22:28:47 -0500

    chore: update switch to pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { paraglideVitePlugin } from '@inlang/paraglide-js';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
